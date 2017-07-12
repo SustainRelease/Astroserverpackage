9114b3cf76c56c24a1a19e534070cc336fcf8e1f
SustainRelease <sirrobbiemuir@hotmail.com>
2017-07-12 14:22:26 -0500

    Changes to build.js

--- bin/build.js
+++ bin/build.js
@@ -1,5 +1,5 @@
 var sites = require('./siteManager.json');
 for (let key in sites) {
-  console.log("Building " + key);
+  console.log("Building " + key + " on port " + sites[key].port);
   require(sites[key].scriptPath)(sites[key].port,sites[key].subRoute);
 }
